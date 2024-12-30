653ebc3502a3e21c430e2e01c6e02cf322de8a79
adriesco <144839808+adriesco@users.noreply.github.com>
2024-12-30 14:47:37 +0100

    Cambios en la constructora y setSettings casi hecho

--- VentanaJava/src/main/Engine.java
+++ VentanaJava/src/main/Engine.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -27,62 +28,6 @@ public class Engine extends JFrame implements ActionListener {
 	 * Constructora
 	 */
 	public Engine() {
-
-		contentPanel = new JPanel(new BorderLayout());
-		displayPanel = new JPanel();
-		buttonPanel = new JPanel(new GridLayout(4, 4, 3, 3));
-		display = new JTextField();
-		display.setHorizontalAlignment(JTextField.RIGHT);
-		display.setEditable(false);
-		display.setFont(new Font("Arial", Font.BOLD, 18));
-
-		displayPanel.setLayout(new BorderLayout());
-		displayPanel.add(display, BorderLayout.CENTER);
-
-		n0 = new JButton("0");
-		n1 = new JButton("1");
-		n2 = new JButton("2");
-		n3 = new JButton("3");
-		n4 = new JButton("4");
-		n5 = new JButton("5");
-		n6 = new JButton("6");
-		n7 = new JButton("7");
-		n8 = new JButton("8");
-		n9 = new JButton("9");
-		divide = new JButton("/");
-		multiply = new JButton("*");
-		subtract = new JButton("-");
-		add = new JButton("+");
-		equal = new JButton("=");
-		reset = new JButton("C");
-
-		JButton[] buttons = { n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, divide, multiply, subtract, add, equal, reset };
-		for (JButton button : buttons) {
-			button.setFont(new Font("Arial", Font.PLAIN, 14));
-		}
-
-		buttonPanel.add(n7);
-		buttonPanel.add(n8);
-		buttonPanel.add(n9);
-		buttonPanel.add(divide);
-		buttonPanel.add(n4);
-		buttonPanel.add(n5);
-		buttonPanel.add(n6);
-		buttonPanel.add(multiply);
-		buttonPanel.add(n1);
-		buttonPanel.add(n2);
-		buttonPanel.add(n3);
-		buttonPanel.add(subtract);
-		buttonPanel.add(n0);
-		buttonPanel.add(reset);
-		buttonPanel.add(equal);
-		buttonPanel.add(add);
-
-		contentPanel.add(displayPanel, BorderLayout.NORTH);
-		contentPanel.add(buttonPanel, BorderLayout.CENTER);
-
-		setContentPane(contentPanel);
-
 		setSettings();
 		addActionEvent();
 	}
@@ -101,8 +46,52 @@ public class Engine extends JFrame implements ActionListener {
 	 * Metodo donde se crea toda la ventana
 	 */
 	private void setSettings() {
-		// TODO Auto-generated method stub
+		this.setTitle("Mi Calculadora");
+		this.setSize(350, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 
+		contentPanel = new JPanel(new BorderLayout());
+		displayPanel = new JPanel(new BorderLayout());
+		buttonPanel = new JPanel(new GridLayout(4, 4, 5, 5));
+
+		display = new JTextField(20);
+		display.setEditable(false);
+		display.setFont(new Font("Verdana", Font.BOLD, 18));
+		displayPanel.add(display);
+
+		n0 = new JButton("0");
+		n1 = new JButton("1");
+		n2 = new JButton("2");
+		n3 = new JButton("3");
+		n4 = new JButton("4");
+		n5 = new JButton("5");
+		n6 = new JButton("6");
+		n7 = new JButton("7");
+		n8 = new JButton("8");
+		n9 = new JButton("9");
+		divide = new JButton("÷");
+		multiply = new JButton("×");
+		subtract = new JButton("−");
+		add = new JButton("+");
+		equal = new JButton("=");
+		reset = new JButton("AC");
+
+		JButton[] buttons = { n7, n8, n9, divide, n4, n5, n6, multiply, n1, n2, n3, subtract, reset, n0, equal, add };
+		for (JButton button : buttons) {
+			if (button == divide || button == multiply || button == subtract || button == add || button == equal) {
+				setFeaturesButton(button, ButtonType.OPERATOR);
+			} else {
+				setFeaturesButton(button, ButtonType.REGULAR);
+			}
+
+			buttonPanel.add(button);
+		}
+
+		contentPanel.add(displayPanel, BorderLayout.NORTH);
+		contentPanel.add(buttonPanel, BorderLayout.CENTER);
+		this.add(contentPanel);
+		this.setVisible(true);
 	}
 
 	/**
